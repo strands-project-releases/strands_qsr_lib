2b90512e328c693987febf8d86327f3e90746a4f
STRANDS Jenkins <strands@hanheide.net>
2015-08-27 12:30:53 +0000

    Imported upstream version '0.2.0' of 'upstream'

--- qsr_lib/src/qsrlib_qsrs/qsr_qtc_bc_simplified.py
+++ qsr_lib/src/qsrlib_qsrs/qsr_qtc_bc_simplified.py
@@ -22,127 +22,78 @@ class QSR_QTC_BC_Simplified(QSR_QTC_Simplified_Abstractclass):
     """Make default QSRs and provide an example for others"""
     def __init__(self):
         super(QSR_QTC_BC_Simplified, self).__init__()
+        self._unique_id = "qtcbcs"
         self.qtc_type = "bc"
-        self.qsr_type = "qtc_bc_simplified"  # must be the same that goes in the QSR_Lib.__const_qsrs_available
         self.all_possible_relations = self.return_all_possible_state_combinations()[0]
-        self.qsr_keys = "qtcbcs"
 
-    def make(self, *args, **kwargs):
-        """Make the QSRs
+    def make_world_qsr_trace(self, world_trace, timestamps, qsr_params, req_params, **kwargs):
+        ret = World_QSR_Trace(qsr_type=self._unique_id)
+        qtc_sequence = {}
+        for t, tp in zip(timestamps[1:], timestamps):
+            world_state_now = world_trace.trace[t]
+            world_state_previous = world_trace.trace[tp]
+            if set(world_state_now.objects.keys()) != set(world_state_previous.objects.keys()):
+                ret.add_empty_world_qsr_state(t)
+                continue # Objects have to be present in both timestamps
+            qsrs_for = self._process_qsrs_for(world_state_now.objects.keys(), req_params["dynamic_args"])
+            for o1_name, o2_name in qsrs_for:
+                between = str(o1_name) + "," + str(o2_name)
+                qtc = np.array([], dtype=int)
+                k = [world_state_previous.objects[o1_name].x,
+                     world_state_previous.objects[o1_name].y,
+                     world_state_now.objects[o1_name].x,
+                     world_state_now.objects[o1_name].y]
+                l = [world_state_previous.objects[o2_name].x,
+                     world_state_previous.objects[o2_name].y,
+                     world_state_now.objects[o2_name].x,
+                     world_state_now.objects[o2_name].y]
+                qtc = self._create_qtc_representation(
+                    k,
+                    l,
+                    qsr_params["quantisation_factor"]
+                )
+                distance = self._get_euclidean_distance(
+                    (world_state_now.objects[o1_name].x,
+                         world_state_now.objects[o1_name].y),
+                    (world_state_now.objects[o2_name].x,
+                         world_state_now.objects[o2_name].y)
+                )
 
-        :param args: not used at the moment
-        :param kwargs:
-                        - input_data: World_Trace
-        :return: World_QSR_Trace
-        """
-        input_data = kwargs["input_data"]
-        ret = World_QSR_Trace(qsr_type=self.qsr_type)
-        timestamps = input_data.get_sorted_timestamps()
-
-        parameters = {
-            "distance_threshold": 1.0,
-            "quantisation_factor": 0.0,
-            "validate": True,
-            "no_collapse": False
-        }
-
-        parameters = self._get_parameters(parameters, **kwargs)
-
-        if kwargs["qsrs_for"]:
-            qsrs_for, error_found = self.check_qsrs_for_data_exist(sorted(input_data.trace[timestamps[0]].objects.keys()), kwargs["qsrs_for"])
-            if error_found:
-                raise Exception("Invalid object combination. Has to be list of tuples. Heard: " + np.array2string(np.array(kwargs['qsrs_for'])))
-        else:
-            qsrs_for = self._return_all_possible_combinations(sorted(input_data.trace[timestamps[0]].objects.keys()))
-
-        if qsrs_for:
-            for p in qsrs_for:
-                between = str(p[0]) + "," + str(p[1])
-                o1_name = p[0]
-                o2_name = p[1]
-                quantisation_factor = parameters["quantisation_factor"]
                 try:
-                    if input_data.trace[0].objects[o1_name].kwargs["quantisation_factor"]:
-                        print "Definition of quantisation_factor in object is depricated. Please use parameters field in dynamic_args in service call."
-                        quantisation_factor = input_data.trace[0].objects[o1_name].kwargs["quantisation_factor"]
-                except:
-                    pass
-                distance_threshold = parameters["distance_threshold"]
-                try:
-                    if input_data.trace[0].objects[o1_name].kwargs["distance_threshold"]:
-                        print "Definition of distance_threshold in object is depricated. Please use parameters field in dynamic_args in service call."
-                        quantisation_factor = input_data.trace[0].objects[o1_name].kwargs["distance_threshold"]
-                except:
-                    pass
-                distances = np.array([])
-                qtc_sequence = np.array([], dtype=int)
-                for t0, t1 in zip(timestamps, timestamps[1:]):
-                    timestamp = t1
-                    try:
-                        k = [input_data.trace[t0].objects[o1_name].x,
-                             input_data.trace[t0].objects[o1_name].y,
-                             input_data.trace[t1].objects[o1_name].x,
-                             input_data.trace[t1].objects[o1_name].y]
-                        l = [input_data.trace[t0].objects[o2_name].x,
-                             input_data.trace[t0].objects[o2_name].y,
-                             input_data.trace[t1].objects[o2_name].x,
-                             input_data.trace[t1].objects[o2_name].y]
-                        qtc_sequence = np.append(qtc_sequence, self._create_qtc_representation(
-                            k,
-                            l,
-                            quantisation_factor
-                        )).reshape(-1,4)
-                        distances = np.append(
-                            distances,
-                            self._get_euclidean_distance(
-                                (input_data.trace[timestamp].objects[o1_name].x,
-                                     input_data.trace[timestamp].objects[o1_name].y),
-                                (input_data.trace[timestamp].objects[o2_name].x,
-                                     input_data.trace[timestamp].objects[o2_name].y)
-                            )
-                        )
-
-                    except KeyError:
-                        ret.add_empty_world_qsr_state(timestamp)
-
-                no_collapse = parameters["no_collapse"]
-                try:
-                    if input_data.trace[0].objects[o1_name].kwargs["no_collapse"]:
-                        print "Definition of no_collapse in object is depricated. Please use parameters field in dynamic_args in service call."
-                        no_collapse = input_data.trace[0].objects[o1_name].kwargs["no_collapse"]
-                except:
-                    pass
-                try:
-                    validate = parameters["validate"]
-                    if input_data.trace[0].objects[o1_name].kwargs["validate"]:
-                        print "Definition of validate in object is depricated. Please use parameters field in dynamic_args in service call."
-                        validate = input_data.trace[0].objects[o1_name].kwargs["validate"]
-                except:
-                    pass
-
-                if not type(no_collapse) is bool or not type(validate) is bool:
-                    raise Exception("'no_collapse' and 'validate' have to be boolean values.")
-
-                qtc_sequence = self._create_bc_chain(qtc_sequence, distances, distance_threshold)
-                if not no_collapse:
-                    qtc_sequence = self._collapse_similar_states(qtc_sequence)
-                if validate:
-                    qtc_sequence = self._validate_qtc_sequence(qtc_sequence)
-                for idx, qtc in enumerate(qtc_sequence):
-                    qsr = QSR(
-                        timestamp=idx+1,
-                        between=between,
-                        qsr=self.qtc_to_output_format((qtc), kwargs["future"])
+                    qtc_sequence[between]["qtc"] = np.append(
+                        qtc_sequence[between]["qtc"],
+                        qtc
+                    ).reshape(-1,4)
+                    qtc_sequence[between]["distances"] = np.append(
+                            qtc_sequence[between]["distances"],
+                            distance
                     )
-                    ret.add_qsr(qsr, idx+1)
+                except KeyError:
+                    qtc_sequence[between] = {
+                        "qtc": qtc,
+                        "distances": np.array([distance])
+                    }
 
-        if no_collapse and not validate:
-            ret = self._rectify_timestamps(input_data, ret)
+        for between, qtcbc in qtc_sequence.items():
+            qtcbc["qtc"] = self._create_bc_chain(qtcbc["qtc"], qtcbc["distances"], qsr_params["distance_threshold"])
+            if not qsr_params["no_collapse"]:
+                qtcbc["qtc"] = self._collapse_similar_states(qtcbc["qtc"])
+            if qsr_params["validate"]:
+                qtcbc["qtc"] = self._validate_qtc_sequence(qtcbc["qtc"])
+            for idx, q in enumerate(qtcbc["qtc"]):
+                qsr = QSR(
+                    timestamp=idx+1,
+                    between=between,
+                    qsr=self.qtc_to_output_format(q)
+                )
+                ret.add_qsr(qsr, idx+1)
 
         return ret
 
     def _create_bc_chain(self, qtc, distances, distance_threshold):
         ret = np.array([])
+        if len(qtc.shape) == 1:
+            qtc = [qtc]
         for dist, state in zip(distances, qtc):
             if dist > distance_threshold:
                 ret = np.append(ret, np.append(state[0:2],[np.nan,np.nan]), axis=0)
@@ -150,7 +101,7 @@ class QSR_QTC_BC_Simplified(QSR_QTC_Simplified_Abstractclass):
                 ret = np.append(ret, state, axis=0)
         return ret.reshape(-1,4)
 
-    def qtc_to_output_format(self, qtc, future=False):
+    def qtc_to_output_format(self, qtc):
         """Overwrite this for the different QTC veriants to select only the parts
         from the QTCC tuple that you would like to return.
         Example for QTCB: return qtc[0:2]
@@ -159,8 +110,8 @@ class QSR_QTC_BC_Simplified(QSR_QTC_Simplified_Abstractclass):
 
         :return: "q1,q2,q4,q5" or {"qtcbcs": "q1,q2,q4,q5"} if future is True
         """
-        s = super(QSR_QTC_BC_Simplified, self).qtc_to_output_format(qtc) if not np.isnan(qtc[2]) else super(QSR_QTC_BC_Simplified, self).qtc_to_output_format(qtc[0:2])
-        return self.handle_future(future, s, self.qsr_keys)
+        s = self.create_qtc_string(qtc) if not np.isnan(qtc[2]) else self.create_qtc_string(qtc[0:2])
+        return self._format_qsr(s)
 
     def _get_euclidean_distance(self, p, q):
         """Calculate the Euclidean distance between points p and q
