367b4f734c552917ea17c559c3f3bcf4b847600f
STRANDS Jenkins <strands@hanheide.net>
2015-08-27 12:30:56 +0000

    Rebase from 'upstream'

--- qsr_lib/src/qsrlib_qsrs/qsr_qtc_c_simplified.py
+++ qsr_lib/src/qsrlib_qsrs/qsr_qtc_c_simplified.py
@@ -20,12 +20,11 @@ class QSR_QTC_C_Simplified(QSR_QTC_Simplified_Abstractclass):
     """Make default QSRs and provide an example for others"""
     def __init__(self):
         super(QSR_QTC_C_Simplified, self).__init__()
+        self._unique_id = "qtccs"
         self.qtc_type = "c"
-        self.qsr_type = "qtc_c_simplified"  # must be the same that goes in the QSR_Lib.__const_qsrs_available
         self.all_possible_relations = self.return_all_possible_state_combinations()[0]
-        self.qsr_keys = "qtccs"
 
-    def qtc_to_output_format(self, qtc, future=False):
+    def qtc_to_output_format(self, qtc):
         """Overwrite this for the different QTC veriants to select only the parts
         from the QTCC tuple that you would like to return.
         Example for QTCB: return qtc[0:2]
@@ -34,5 +33,4 @@ class QSR_QTC_C_Simplified(QSR_QTC_Simplified_Abstractclass):
 
         :return: "q1,q2,q4,q5" or {"qtccs": "q1,q2,q4,q5"} if future is True
         """
-        s = super(QSR_QTC_C_Simplified, self).qtc_to_output_format(qtc)
-        return self.handle_future(future, s, self.qsr_keys)
+        return self._format_qsr(self.create_qtc_string(qtc))
